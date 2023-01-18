d909984e96a47bc6f4c325c630ea31ec10f48190
SONU <smalik2811@gmail.com>
2023-01-18 12:15:39 +0530

    Done with the practical

--- Practical 1/main.py
+++ Practical 1/main.py
@@ -1,4 +1,6 @@
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 from People import *
 
 # Getting file name
@@ -7,15 +9,36 @@ text_file_path = current_dir + "/people.txt"
 
 # Reading File
 text_file = open(text_file_path,'r')
-data_set = text_file.readlines()
-data_set = data_set[1:]
+record = text_file.readline()
+record = text_file.readline()
 
-for record in data_set:
+people_summary= {True: 0, False: 0}
+labels = ["Ruleset Passed", "Ruleset Failed"]
+
+while record != "":
     if record[-1] == "\n":
         record = record[0:-1]
+
     print(record, "~ ", end="")
     data_Object = record.split(" ")
+
     peopleObject = People(data_Object[0], data_Object[1], data_Object[2], data_Object[3], data_Object[4])
     result = peopleObject.validateE()
+
+    people_summary[result] += 1
     print(result)
+
     del peopleObject
+    record = text_file.readline()
+
+text_file.close()
+
+# Summary
+print("-----Summary-----")
+print("Number of records Passed Ruleset E: ", people_summary[True])
+print("Number of records Failed Ruleset E: ", people_summary[False])
+
+# Visualistion
+data = np.array([people_summary[True], people_summary[False]])
+plt.pie(data, labels = labels, shadow = True)
+plt.show()
